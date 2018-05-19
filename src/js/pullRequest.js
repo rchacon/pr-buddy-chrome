d452387ed0133dabc17ea4ffb300c3455010bd0b
Raul Chacon <chaconrj@gmail.com>
2018-05-18 22:24:59 -0400

    rename var

--- src/js/pullRequest.js
+++ src/js/pullRequest.js
@@ -107,21 +107,21 @@ var pullRequest = {
    * https://www.w3schools.com/howto/howto_js_sort_list.asp
    */
   sortList: function (list) {
-    var i, switching, b, shouldSwitch;
+    var i, switching, items, shouldSwitch;
     switching = true;
     /* Make a loop that will continue until
     no switching has been done: */
     while (switching) {
       // Start by saying: no switching is done:
       switching = false;
-      b = list.getElementsByTagName("li");
+      items = list.getElementsByTagName("li");
       // Loop through all list items:
-      for (i = 0; i < (b.length - 1); i++) {
+      for (i = 0; i < (items.length - 1); i++) {
         // Start by saying there should be no switching:
         shouldSwitch = false;
         /* Check if the next item should
         switch place with the current item: */
-        if (b[i].innerHTML.toLowerCase() > b[i + 1].innerHTML.toLowerCase()) {
+        if (items[i].innerHTML.toLowerCase() > items[i + 1].innerHTML.toLowerCase()) {
           /* If next item is alphabetically lower than current item,
           mark as a switch and break the loop: */
           shouldSwitch = true;
@@ -131,7 +131,7 @@ var pullRequest = {
       if (shouldSwitch) {
         /* If a switch has been marked, make the switch
         and mark the switch as done: */
-        b[i].parentNode.insertBefore(b[i + 1], b[i]);
+        items[i].parentNode.insertBefore(items[i + 1], items[i]);
         switching = true;
       }
     }
